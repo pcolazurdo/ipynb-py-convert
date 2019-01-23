4b8dd51be14d85b2d605071b43f62b781452c87e
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-01-23 15:25:23 +0700

    docs fix

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
     name='ipynb-py-convert',
     packages=['ipynb_py_convert'],
-    version='0.4',
+    version='0.4.1',
     description='Convert .py files runnable in VSCode/Python or Atom/Hydrogen to jupyter .ipynb notebooks and vice versa',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Noj Vek',
     author_email='nojvek@gmail.com',
     license='MIT',
-    url='https://github.com/kiwi0fruit/ipynb-py-converter',
+    url='https://github.com/kiwi0fruit/ipynb-py-convert',
     keywords=['vscode', 'jupyter', 'convert', 'ipynb', 'py', 'atom', 'hydrogen'],
     classifiers=[],
     entry_points={
